725f3cde064722ed255cc573919221f7f7ca409c
arunpasc <arunprakash.emailid@gmail.com>
2026-07-18 15:18:47 +0530

    changed

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello from langchain-course!")
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
 
+load_dotenv()
+
+
+def main():
+    information = """Tendulkar"""
+
+    prompt_template = """You are a cricket expert. Answer about {information} in just 2 lines."""
+
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"],
+        template=prompt_template,
+    )
+
+    LLM = ChatOpenAI(model="gpt-5", temperature=0.9, max_tokens=1000)
+    chain = summary_prompt_template | LLM
+    result = chain.invoke({"information": information})
+    return(result.content)
 
 if __name__ == "__main__":
-    main()
+    response = main()
+    print(response)
