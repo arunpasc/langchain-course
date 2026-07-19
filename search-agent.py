3134c838aa523f175fcc7f26031ba2878c70c44f
arunpasc <arunprakash.emailid@gmail.com>
2026-07-19 10:58:14 +0530

    All done

--- search-agent.py
+++ search-agent.py
@@ -7,16 +7,24 @@ from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
 from tavily import TavilyClient
 from langchain_tavily import TavilySearch
+from pydantic import BaseModel, Field
 
 
 
 load_dotenv()
 
+class Source(BaseModel):
+    url: str = Field(description="The URL of the source")
+
+class AgentResponse(BaseModel):
+    answer: str = Field(description="The answer to the user's question")
+    sources: list[Source] = Field(description="A list of sources used to generate the answer")
+
 
 
 llm = ChatOpenAI(model="gpt-5", temperature=0.9)
 tools = [TavilySearch()]
-agent = create_agent(model=llm, tools=tools)
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
 def main():
     result = agent.invoke(
